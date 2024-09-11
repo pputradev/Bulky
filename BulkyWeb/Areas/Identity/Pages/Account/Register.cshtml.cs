fffea65ef06305aaddb4e07cb2e5aa47e4c1b1a5
Premeela Putra <pputradevops@gmail.com>
2024-09-11 18:06:23 +0530

    4th day  after identity

--- BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BulkyWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -108,6 +108,13 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
             public string? Role {  get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? State { get; set; }
+            public string? City { get; set; }
+            public string? Zipcode { get; set; }
+            public string? PhoneNumber { get; set; }
 
         }
 
@@ -141,12 +148,26 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
+                user.StreetAddress = Input.StreetAddress;
+                user.PhoneNumber = Input.PhoneNumber;
+                user.City = Input.City;
+                user.Zipcode = Input.Zipcode;
+                user.State = Input.State;
+                user.Name = Input.Name;
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    if (!string.IsNullOrEmpty(Input.Role))
+                    {
+                        await _userManager.AddToRoleAsync(user, Input.Role);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, SD.Role_User_Customer);
+                    }
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
